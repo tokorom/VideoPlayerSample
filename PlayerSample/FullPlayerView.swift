43a54b052370b73e9480c181299a54e8de3c96d8
tokorom <tokorom@gmail.com>
2022-09-22 15:34:38 +0900

    Improve FullPlayerView

--- PlayerSample/FullPlayerView.swift
+++ PlayerSample/FullPlayerView.swift
@@ -14,6 +14,7 @@ struct FullPlayerView: View {
                 Color.black
                 VideoPlayer(player: player)
             }
+            .ignoresSafeArea()
         } else {
             Spacer()
         }
