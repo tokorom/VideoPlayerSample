4a266385efda938f14589ea06e2b769185e883b0
tokorom <tokorom@gmail.com>
2022-09-22 15:08:12 +0900

    First commit

---
+++ PlayerSample/PlayerSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerSampleApp.swift
+//  PlayerSample
+//
+//  Created by Yuta Tokoro on 2022/09/22.
+//
+
+import SwiftUI
+
+@main
+struct PlayerSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
